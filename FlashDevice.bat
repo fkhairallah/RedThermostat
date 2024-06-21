7e70ecbe79d57e1c02f1aa5eff6408ba69648ce8
Fady <fady@deligent.com>
2024-06-21 13:03:15 -0400

    flashing

---
+++ FlashDevice.bat
@@ -0,0 +1,1 @@
+python C:\Users\fady\Documents\Development\PlatformIO\homie-esp8266\scripts\ota_updater\ota_updater.py -l 192.168.68.10  -p 1883 -t "RED/" -i red-thermostat-den .\.pio\build\local\firmware.bin
